4a5daa74abb6e64be43e1d40ad002a03fc75ec6e
Yaprak <yaprak.ayazoglu@gmail.com>
2015-04-16 21:49:56 +0200

    init structure

---
+++ dev/Gulpfile.js
@@ -0,0 +1,22 @@
+var gulp  = require('gulp'),
+    sass  = require('gulp-sass');
+
+// Standard handler
+function standardHandler(err) {
+    // Log to console
+    console.log(err.message);
+    this.emit('end');
+}
+
+gulp.task('sass', function() {
+    return gulp.src('./scss/style.scss')
+        .on('error', standardHandler)
+        .pipe(sass(({sourcemap: true})))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watcher', ['sass'] , function() {
+    gulp.watch('./scss/*.scss', ['sass'] );
+});
+
+gulp.task('default', ['sass']);
